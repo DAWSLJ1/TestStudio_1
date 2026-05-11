3133f0f10c047074b246019a9d6e8b01fd3ec7e6
Zac Woods (1000140251) <WOODZP1@student.op.ac.nz>
2026-05-12 10:40:46 +1200

    added lab 10 to the file

--- Program.cs
+++ Program.cs
@@ -5,6 +5,12 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            //Zac lab 10
+            Random random = new Random();
+            int[] dice = new int[6];
+            for (int i = 0; i < 6000; i++) dice[random.Next(6)]++;
+            Console.WriteLine($"results \t1\t2\t3\t4\t5\t6\n6000 \t\t{dice[0]}\t{dice[1]}\t{dice[2]}\t{dice[3]}\t{dice[4]}\t{dice[5]}");
+            Console.ReadLine();
         }
     }
 }
