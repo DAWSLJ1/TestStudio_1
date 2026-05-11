8d61c3ee407c15b5219cf3755812efac7b9ba4ad
Abby Ashton (1000140876) <ASHTWJ1@student.op.ac.nz>
2026-05-12 10:36:55 +1200

    Added Abby section

--- Program.cs
+++ Program.cs
@@ -5,6 +5,25 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            // Abby's section
+            char start, stop;
+            string temp;
+
+            Console.Write("What is your starting character? ");
+            temp = Console.ReadLine();
+            start = Convert.ToChar(temp);
+
+            Console.Write("What is your ending character? ");
+            temp = Console.ReadLine();
+            stop = Convert.ToChar(temp);
+
+            for (char i = start; (int)i < ((int)stop + 1); i++)
+            {
+                Console.Write(i + " ");
+            }
+
+            Console.ReadLine();
         }
     }
 }
